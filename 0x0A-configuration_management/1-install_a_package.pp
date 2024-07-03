92602d79c6c0b9b464da1adad5a495f3852af828
chierz <bonanwaeze@gmail.com>
2024-07-02 18:01:20 -0700

    1-puppet install packages

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,19 @@
-#using puppet to install apackage
+#using puppet to install a package
 
-#!/usr/bin/pup
-# Install a specific version of falsk (2.1.0)
-package { 'flask':
+# Install a specific version of python3(3.8.10), flask(2.1.0) & Werkzeug(2.1.1)
+
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
